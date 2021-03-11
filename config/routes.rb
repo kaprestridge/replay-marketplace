b46465ae84cf10876d920cd7b326315d4eb2687b
kaprestridge <kaprestridge@gmail.com>
2021-03-11 14:18:34 +1100

    index contents

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :listings
   root "pages#index"
   get "/faq", to: 'pages#faq'
+  get "/meetups", to: 'pages#meetups'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
