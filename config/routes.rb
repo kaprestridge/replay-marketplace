038d350bb06b5828f630969a2d865183bfcd4723
kaprestridge <kaprestridge@gmail.com>
2021-03-11 12:37:33 +1100

    feature: navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   root "pages#index"
+  get "/faq", to: 'pages#faq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
