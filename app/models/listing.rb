783e9c7cfb65c48e5cd1449d41e20c3297c8b320
kaprestridge <kaprestridge@gmail.com>
2021-03-20 15:12:41 +1100

    added usernames to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,15 @@
 class Listing < ApplicationRecord
     has_one_attached :photo
+    belongs_to :user
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def listing_params
+      params.require(:listing).permit(:title, :description, :lost_date, :completed, :image, :username)
+    end
 end
