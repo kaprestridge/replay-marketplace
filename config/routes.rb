d532dd12ac1f399d04e7887af3b9819c99dfb955
kaprestridge <kaprestridge@gmail.com>
2021-03-20 17:21:04 +1100

    search function on listings page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root "pages#index"
   get "/faq", to: 'pages#faq'
   get "/meetups", to: 'pages#meetups'
+  get '/search' => 'listings#search', :as => 'search_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
